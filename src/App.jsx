f46a723625b1627fe8f1f735821f98ff806911cf
João <jpedross1999@gmail.com>
2019-10-19 07:03:24 -0300

    Feat: Tela campanhas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import { localStorageUtils } from 'utils'
 import { HackathonLoader } from 'generics'
-import { Home, TaskCompleted, PlaceDetail, Login, Campaign, MainPage, SignUp } from 'scenes'
+import { Home, TaskCompleted, PlaceDetail, Login, Campaign, MainPage, SignUp, CampaignManager } from 'scenes'
 
 import './App.scss'
 
@@ -28,6 +28,7 @@ class App extends Component {
             <Route path="/completed" component={TaskCompleted} />
             <Route path="/detail" component={PlaceDetail} />
             <Route path="/campaign" component={Campaign} />
+            <Route path="/campaign_manager" component={CampaignManager} />
             <Route path="/" component={Login} />
             <Route path="/signup" component={SignUp} />
           </Switch>
